9c8ce6bae1334a2cd7e810e4661d702c2f209567
F4stZ4p <None>
2019-07-23 22:34:29 +0100

    .

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 echo Lavalink on Heroku by F4stZ4p
 echo Credit to diniboy for Lavalink init script. Line 10
 
-echo Downloading latest release of Lavalink...
-curl -s https://api.github.com/repos/Frederikam/Lavalink/releases/latest | grep browser_download_url | cut -d '"' -f 4 | wget -qi -
+echo Downloading latest release of Lavalink from GitHub...
+curl -s https://api.github.com/repos/Frederikam/Lavalink/releases/latest | grep browser_download_url | cut -d '""' -f 4 | wget -qi -
 
 echo Starting Lavalink...
 sed -i "s|2333|$PORT|" application.yml; java -jar Lavalink.jar
