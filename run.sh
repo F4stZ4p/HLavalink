6ad07f165522a913488ebc03491af0c6d7bb7f25
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2019-07-24 00:36:28 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo Lavalink on Heroku by F4stZ4p
+echo Credit to diniboy for Lavalink init script
+
+echo Downloading latest release of Lavalink...
+curl -s https://api.github.com/repos/Frederikam/Lavalink/releases/latest \
+  | grep browser_download_url \
+  | grep Lavalink \
+  | cut -d '"' -f 4 \
+  | wget -qi -
+ 
+echo Starting Lavalink...
+sed -i "s|2333|$PORT|" application.yml; java -jar Lavalink.jar
