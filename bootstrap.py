eeda9a363318d958b61446ca57636d5dcb79c32e
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2020-06-23 15:57:02 +0300

    Fix issues with HLavalink (thanks chr1s#0002)

--- bootstrap.py
+++ bootstrap.py
@@ -28,9 +28,13 @@ class LavalinkBootstrap:
         
         self.prepare_version_number() # Fixes #1
         
-        self.download_command = f"curl https://github.com/Frederikam/Lavalink/releases/download/{self._version_number}/Lavalink.jar -O"
-        print(f"Download command: {self.download_command}")
-
+        self.use_dev_lavalink = True if str(environ.get("USE_DEV_LAVALINK")).lower() not in ("no", "0", "n") else False
+        if self.use_dev_lavalink:
+            
+            print("[INFO] Using developer Lavalink version")
+        
+        self.download_command = f"curl -L https://ci.fredboat.com/repository/download/Lavalink_Build/8110:id/Lavalink.jar?guest=1 -o Lavalink.jar" if self.use_dev_lavalink else f"curl -L https://github.com/Frederikam/Lavalink/releases/download/{self._version_number}/Lavalink.jar -O"
+        print(f"[INFO] Download command: {self.download_command}")
         
         self.replace_port_command = 'sed -i "s|DYNAMICPORT|$PORT|" application.yml'
 
