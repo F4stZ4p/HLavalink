756d84c58930d241cd204ea1f1d76fb00061afab
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2020-07-31 20:29:34 +0300

    Bump lavalink to the latest version

--- bootstrap.py
+++ bootstrap.py
@@ -33,7 +33,7 @@ class LavalinkBootstrap:
             
             print("[INFO] Using developer Lavalink version")
         
-        self.download_command = f"curl -L https://ci.fredboat.com/repository/download/Lavalink_Build/8110:id/Lavalink.jar?guest=1 -o Lavalink.jar" if self.use_dev_lavalink else f"curl -L https://github.com/Frederikam/Lavalink/releases/download/{self._version_number}/Lavalink.jar -O"
+        self.download_command = f"curl -L https://ci.fredboat.com/repository/download/Lavalink_Build/8231:id/Lavalink.jar?guest=1 -o Lavalink.jar" if self.use_dev_lavalink else f"curl -L https://github.com/Frederikam/Lavalink/releases/download/{self._version_number}/Lavalink.jar -O"
         print(f"[INFO] Download command: {self.download_command}")
         
         self.replace_port_command = 'sed -i "s|DYNAMICPORT|$PORT|" application.yml'
